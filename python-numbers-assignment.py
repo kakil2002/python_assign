1320bf623eaa7695ecc16cb7b8deb4d4384752aa
k akil <167415512+kakil2002@users.noreply.github.com>
2024-06-09 14:56:22 +0530

    Add files via upload

---
+++ python-numbers-assignment.py
@@ -0,0 +1,16 @@
+PI=3.14
+x=75
+print(PI,type(PI),sep=',')
+print(x,type(x),sep=',')
+
+x=1+2j
+y=1+2J
+print(x,type(x),sep=',')
+print(y,type(y),sep=',')
+
+
+import sys
+x=123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789
+print("value is :",x)
+print("size is :",sys.getsizeof(x))
+print("type is :",type(x))
---
+++ strings-assignment.py
@@ -0,0 +1,10 @@
+s="Semantic segmentation of Lung Cancer from Chest X-Ray"
+print(s[48])
+
+s1="I have to Love Python Programming"
+print(s1[-23])
+
+#prove string is immutable
+s2="i love python"
+s2[2]='L' #'str' object does not support item assignment
+print(s2)
