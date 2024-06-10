2f8ae95b0d2a6ec1c1f063b40af22acfb5d60161
k akil <167415512+kakil2002@users.noreply.github.com>
2024-06-10 10:27:58 +0530

    2nd assignment

--- list-nested-list-assignment.py
+++ list-nested-list-assignment.py
@@ -3,10 +3,10 @@ print(L1[-4])
 L2 = [28, 31, 'Jython', 'Python', 48, 765, 2201, 5, 2034, 'Java']
 print(L2[3])
 L3 = [21, 9.8, 'hi', 45, 765, 2001, 5, 'Hey']
-print(L3[-6][1])
+print(L3[-6][-1])
 L4 = [21, [4, 5, 6], ['hi', 'hello', [3, 4, 5]], 45, 765, 2001, 5, 2034, 'Jai']
 print(L4[1][0])
 L5 = [21, ['Anil', 'Jai', 'Education', ['Java', 'Kova']], 7065, 2001, 5, 2034, ['Hey', 'Hi'], [1, 2]]
-print(L5[-7][2][2])
+print(L5[-7][-2][-7])
 L6 = [21, ['Anil', 'Education', [['Java', 'Kova'], ['Programming', 'Sugar', 'Sweet', 'Wheat']]], 7065, 5, 2034, [1, 2]]
 print(L6[1][2][1][2])
